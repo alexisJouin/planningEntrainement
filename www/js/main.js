1dfd816208413183a410c7ae60b8d3161468d0bb
Alexis Jouin <a.jouin@stereograph.fr>
2016-09-30 15:53:53 +0200

    ADD : librairies uikit et jqwidget ADD : dateScrolling (statique) ADD : bouton d'édition du compte

--- www/js/main.js
+++ www/js/main.js
@@ -2,7 +2,34 @@ $(document).ready(function () {
     $("#creationMove").click(function () {
         window.location.href = "creationGroup.php";
     });
-    
+
+    $("#EditProfilMove").click(function () {
+        window.location.href = "EditionCompte.php";
+    });
+
+
+    $(function () {
+        $('#dateScrolling').jqxScrollView({width: 600, height: 450, buttonsOffset: [0, 0]});
+        $('#StartBtn').jqxButton({theme: theme});
+        $('#StopBtn').jqxButton({theme: theme});
+        $('#StartBtn').click(function () {
+            $('#dateScrolling').jqxScrollView({slideShow: true});
+        });
+        $('#StopBtn').click(function () {
+            $('#dateScrolling').jqxScrollView({slideShow: false});
+        });
+    });
+
+    $('#yes').click(function () {
+        console.log("Test Yes! ");
+        UIkit.notify({
+            message: 'yES §§	 !',
+            status: 'info',
+            timeout: 2000,
+            pos: 'top-center'
+        });
+    });
+
 //     alert($(".derby_name_session"));
 
 
--- www/main.php
+++ www/main.php
@@ -13,13 +13,24 @@
     echo(session_status());
     
 ?>
-       
+    <script src="js/uikit/uikit.min.js"></script>
+    <link rel="stylesheet" href="css/uikit/jqx.base.css"/>
+    <link rel="stylesheet" href="css/uikit/jqx.arctic.css" />
+    <script type="text/javascript" src="js/uikit/demos.js"></script>
+    <script type="text/javascript" src="js/uikit/jqxcore.js"></script>
+    <script type="text/javascript" src="js/uikit/jqxscrollview.js"></script>
+    <script type="text/javascript" src="js/uikit/jqxbuttons.js"></script>
+    <script type="text/javascript" src="js/uikit/jqxnotification.js"></script>
+    <script type="text/javascript" src="js/uikit/jqxlistbox.js"></script>
+    
     <title>Menu Principal</title>
     </head>
     <body>
         
         <p>Bonjour <?php echo  $_SESSION['id']; ?></p>
 
+        <button id="EditProfilMove">Votre Porfil</button>
+        
         <!-- Cas où l'untilisateur n'a aucune affiliation -->
         <div id='firstUse'>
 
@@ -30,20 +41,32 @@
                 <span>ETC ....</span>
             </div>
 
-            <footer>
-                <p>
-                    Vous êtes le coach ou le responsable de la gestion des
-                    entrainements ? Créez votre groupe dès maintenant
-                </p>
-                <button id="creationMove">Créer Groupe</button>
-            </footer>
         </div>
         
+        <!-- Cas où l'untilisateur possède un groupe -->
         <div id='planning'>
-            <p>Pour le prochain entraînement du ?? Serez-vous présent ?</p>
-            <button>OUI</button>
-            <button>PEUT-ÊTRE</button>
-            <button>NON</button>
+            <div id="dateScrolling">
+                <!--Exemple a la con-->
+                <div><h1>Jeudi 22 Septembre 2016</h1><h2>Entrainement à 18h30</h2></div>
+                <div><h1>Samedi 30 Septembre 2016</h1><h2>Entrainement à 15h30</h2></div>
+                <div><h1>Jeudi 05 Octobre 2016</h1><h2>Entrainement à 18h30</h2></h1></div>
+            </div>
+            <div style='margin-top: 20px;'></div>
+            <div style='margin-top: 20px;'></div>
+            <br />
+            <div style="display: inline-block">
+                <button value="Oui" id="yes" data-message="Super !">Oui</button>
+                <button value="YN" id="yes" data-message="Super !" >Peut-être</button>
+                <button value="Non" id="no" data-message="Oh non !">Non</button>
+            </div>
         </div>
+        
+        <footer>
+            <p>
+                Vous êtes le coach ou le responsable de la gestion des
+                entrainements ? Créez votre groupe dès maintenant
+            </p>
+            <button id="creationMove">Créer Groupe</button>
+        </footer>
     </body>
 </html>
